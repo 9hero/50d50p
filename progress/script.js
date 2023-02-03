e609f037f0067fcc8912ab5ea4a49fbe75c3d2a3
9hero <rngnlah@naver.com>
2023-02-03 14:17:41 +0900

    start

---
+++ progress/script.js
@@ -0,0 +1,34 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+prev.addEventListener('click',()=>{
+    currentActive--
+
+    if(currentActive < 1){
+        currentActive == 1
+    }
+
+    update()
+})
+
+next.addEventListener('click',()=>{
+    currentActive++
+
+    if(currentActive > circles.length){
+        currentActive == circles.length
+    }
+})
+
+function update(){
+    circles.forEach((circle,idx) => {
+        if(idx < currentActive){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    });
+}
